675fcb107e3ffe050cf957f63437b16c78a4b8aa
shun-nagayama <allandarp.shun@gmail.com>
2019-08-21 22:11:44 +0900

    linebot

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import os
 import requests
+import url_get
 from bs4 import BeautifulSoup
 from flask import Flask, request, abort
 from linebot import (
@@ -23,24 +24,30 @@ line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 @app.route("/callback", methods = ['POST'])
-def get_href(x):
+def callback():
+    # リクエストヘッダーから署名検証のための値を取得します。
+    signature = request.headers['X-Line-Signature']
+ 
+    # リクエストボディを取得します。
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+ 
+    # handle webhook body
+　# 署名を検証し、問題なければhandleに定義されている関数を呼び出す。
+    try:
+        handler.handle(body, signature)
+　# 署名検証で失敗した場合、例外を出す。
+    except InvalidSignatureError:
+        abort(400)
+　# handleの処理を終えればOK
+    return 'OK'
 
-    i = 201
-    while i < 221:
-        url = 'https://www.jma.go.jp/jp/yoho/' + str(i) + '.html'
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text,'lxml')
-        links = soup.findAll('area')
-        for link in links:
-            href, title = link.get('href'), link.get('title')
-            if x == title:
-                break
-        else:
-            i += 1
-            continue
-        break
-
-    return href
+@handler.add(FollowEvent)
+def handle_follow(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='Hi')
+    )
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -48,7 +55,7 @@ def handle_message(event):
     #print("天気予報(地名を入力)")
     region = event.message.text
 
-    number = get_href(region)
+    number = url_get.get_href(region)
 
     #print(number)
     
@@ -84,22 +91,22 @@ def handle_message(event):
     max = bsObj.find(class_="max")
     temp_max = max.text
 
-
-
     #出力
-    #print("天気予報:{}".format(area))
-    print("天気予報:{}県".format(region))
-    print()
-    print("天気:")
-    print("{}".format(today_weather))
-    print("詳細:")
-    print("{}".format(today_info))
-    print()
-    print("降水確率:{}".format(today_rain))
-    print("気温")
-    print("都市:{}".format(temp_city))
-    print("最高気温:{}".format(temp_max))
-    print("最低気温:{}".format(temp_min))
+    line_bot_api.reply_message(
+        #print("天気予報:{}".format(area))
+        print("天気予報:{}県".format(region))
+        print()
+        print("天気:")
+        print("{}".format(today_weather))
+        print("詳細:")
+        print("{}".format(today_info))
+        print()
+        print("降水確率:{}".format(today_rain))
+        print("気温")
+        print("都市:{}".format(temp_city))
+        print("最高気温:{}".format(temp_max))
+        print("最低気温:{}".format(temp_min))
+    )
 
 if __name__ == "__main__":
     #print_tenki()
--- url_get.py
+++ url_get.py
@@ -2,6 +2,26 @@
 import requests
 from bs4 import BeautifulSoup
 
+def get_href(x):
+
+    i = 201
+    while i < 221:
+        url = 'https://www.jma.go.jp/jp/yoho/' + str(i) + '.html'
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text,'lxml')
+        links = soup.findAll('area')
+        for link in links:
+            href, title = link.get('href'), link.get('title')
+            if x == title:
+                break
+        else:
+            i += 1
+            continue
+        break
+
+    return href
+
+'''
 def get():
     for i in range(201, 220):
         url = 'https://www.jma.go.jp/jp/yoho/' + str(i) + '.html'
@@ -10,6 +30,7 @@ def get():
         links = soup.findAll('area')
         for link in links:
                 print(link.get('href'), link.get('title'))
+'''
 
 '''
 response = requests.get('https://www.jma.go.jp/jp/yoho/201.hmtl')
